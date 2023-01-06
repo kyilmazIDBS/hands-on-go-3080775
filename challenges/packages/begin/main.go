406dae4d07144e99ecc723ee6e83e80246a656f4
kyilmazIDBS <121097603+kyilmazIDBS@users.noreply.github.com>
2023-01-06 15:58:07 +0000

    first day

--- challenges/packages/begin/main.go
+++ challenges/packages/begin/main.go
@@ -1,10 +1,16 @@
 // challenges/packages/begin/proverbs.go
 package main
-
 // import the proverbs package
+import "fmt"
+import "github.com/jboursiquot/go-proverbs"
 
 // getRandomProverb returns a random proverb from the proverbs package
 
+func getRandomProverb(){
+	fmt.Println(proverbs.Random().Saying)
+}
+
 func main() {
 	// print the result of calling your getRandomProverb function
+	getRandomProverb()
 }
--- packages/basics/main.go
+++ packages/basics/main.go
@@ -1,5 +1,13 @@
 // packages/basics/main.go
 package main
 
+import (
+	"fmt"
+	"time"
+)
 func main() {
+
+	fmt.Println("Hello Gopher!")
+   
+	fmt.Printf("Today is %s",time.Now().Weekday())
 }
